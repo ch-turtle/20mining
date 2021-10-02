9ce84d51054354260f1c2c96eb36ecfbbd2f294c
ch-turtle <91691082+ch-turtle@users.noreply.github.com>
2021-10-02 13:01:21 +0200

    Update getArticles.py

--- getArticles.py
+++ getArticles.py
@@ -3,14 +3,17 @@ from pandas import json_normalize
 import pandas as pd
 import requests
 import pymongo
-
+import datetime
 
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
+mydb = myclient["20mining"]
 
 rss_url='https://partner-feeds.20min.ch/rss/20minuten'
 art_url = 'https://feed-prod.unitycms.io/6/content/'
+
+
+d = datetime.datetime.now()
 
 # Read feed xml data
 news_feed = feedparser.parse(rss_url) 
@@ -26,15 +29,21 @@ urls = df_news_feed['link'].values
 for i in urls:
     id = i.split("/")[3]
     print(id)
-    story = { "_id": id}
+    story = { "_id": id, "crawl": 0, "time": d}
     # If new article id: write to db and safe article, else pass
-    try:
-        stories = mydb["stories"]
-        stories.insert_one(story)
-        articles = mydb["articles"]
-        r = requests.get(art_url+id)
-        r.encoding = 'utf-8'
-        stories.insert_one(r.json())
-    except:
-        pass
+    stories = mydb["stories"]
+    stories.insert_one(story)
+    articles = mydb["articles"]
+    r = requests.get(art_url+id)
+    r.encoding = 'utf-8'
+    articles.insert_one(r.json())
     
+
+    
+
+
+
+
+	
+	
+	
